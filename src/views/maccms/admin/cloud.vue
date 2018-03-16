7989da52e8a1f819ad4916bff0a922e8b52fb444
supbose <yshxw@qq.com>
2018-03-17 03:54:50 +0800

    整理云

--- src/views/maccms/admin/cloud.vue
+++ src/views/maccms/admin/cloud.vue
@@ -1,14 +1,159 @@
 <style lang="less" scoped>
 @import "./admin.less";
+.ad {
+  float: right;
+}
 </style>
 <template>
     <div class="cloud">
-        <h1>cloud</h1>
+        <Card span="24">
+            <p slot="title">
+                <i class="ivu-icon ivu-icon-ios-cloud-outline">云端采集资源数据库</i>
+            </p>
+
+            <a href="#" slot="extra" @click.prevent="addcloud">
+                <Icon type="ios-cloud-upload"></Icon>
+            </a>
+
+            <div>
+                <Row>
+                    <Col span="24" class="yz-left demo-spin-col">
+                    <Spin fix class="ivu-spin-fix" size="large" v-if="spinShow">
+                        <Icon type="load-c" size=18 class="demo-spin-icon-load"></Icon>
+                        <div>数据加载中...</div>
+                    </Spin>
+                    <Table height="500" border :columns="cloudtable" no-data-text=" " :data="Serverdata">
+                    </Table>
+                    </Col>
+                </Row>
+            </div>
+        </Card>
     </div>
 </template>
 
 <script>
+import expandRow from "./cloud-expand.vue";
 export default {
-  name: "cloud"
+  name: "cloud",
+  components: {
+    expandRow
+  },
+  data() {
+    this.$Message.config({
+      top: 300,
+      duration: 2
+    });
+    return {
+      spinShow: true,
+      cloudtable: [
+        {
+          title: "资源ID",
+          key: "id",
+          width: 100
+          // fixed: "left"
+        },
+        {
+          title: "资源名",
+          key: "name",
+          width: 200
+        },
+        {
+          title: "资源地址",
+          key: "url"
+        },
+        {
+          title: "查看此资源",
+          type: "expand",
+          width: 100,
+          render: (h, params) => {
+            return h(expandRow, {
+              props: {
+                row: params.row
+              }
+            });
+          }
+        },
+        {
+          title: "操作",
+          width: 110,
+          // fixed: "right",
+          render: (h, params) => {
+            return h("div", [
+              h(
+                "Poptip",
+                {
+                  props: {
+                    confirm: true,
+                    title: "您确定要删除这条数据吗?",
+                    transfer: true
+                  },
+                  on: {
+                    "on-ok": () => {
+                      //  this.Adremove(params.index);
+                    }
+                  }
+                },
+                [
+                  h(
+                    "Button",
+                    {
+                      style: {
+                        // marginRight: "5px"
+                      },
+                      props: {
+                        type: "warning",
+                        size: "small",
+                        placement: "top"
+                      }
+                    },
+                    "获取此资源"
+                  )
+                ]
+              )
+            ]);
+          }
+        }
+      ],
+      Serverdata: [],
+      CloudService: {
+        all: "http://yazhitv.duapp.com/api/maccms/all",
+        add: "http://yazhitv.duapp.com/api/maccms/add",
+        obtain: "http://yazhitv.duapp.com/api/maccms/obtain"
+      }
+    };
+  },
+  mounted() {
+    this.all();
+  },
+  methods: {
+    addcloud() {
+      console.log("添加云资源");
+    },
+    all() {
+      var _this = this;
+      _this.spinShow = true;
+      var params = new URLSearchParams();
+      params.append("token", "yazhi.tv");
+      setTimeout(function() {
+        _this.$axios
+          .post(_this.CloudService.all, params, {
+            withCredentials: false
+          })
+          .then(function(res) {
+            console.log("res:" + JSON.stringify(res.data));
+            if (res.data.state === "Success") {
+              _this.Serverdata = res.data.data;
+              _this.spinShow = false;
+            } else {
+              _this.$Message.error("数据获取失败");
+            }
+          })
+          .catch(function(error) {
+            _this.$Message.error("网络链接失败");
+            console.log(error);
+          });
+      }, 1000);
+    }
+  }
 };
 </script>
